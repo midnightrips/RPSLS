41372ae59ed31109c5df4f7af9c1ed72e10ddef2
midnightrips <sg3584@alum.barnard.edu>
2023-08-29 14:47:55 -0400

    Creating Human Player Class

---
+++ RPSLS/HumanPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class HumanPlayer : Player
+    {
+        //member variables
+
+        //constructor
+
+        //member methods
+        public override void ChooseGesture()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
